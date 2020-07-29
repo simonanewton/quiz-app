8e470782480f347abc170dce6c05d6af592cf0e1
Simon Newton <simonanewton@gmail.com>
2020-07-29 03:56:09 -0400

    updated Header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
-import Header from "./components/Header"
+import Header from "./components/Header";
 import './App.css';
 
 function App() {
