587677bef22b1f43a0b45568a2cbf89ce3942615
tonidavisj <toni@tonidavisj.com>
2020-07-30 18:59:53 -0400

    tonid v3

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
 import './App.css';
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
 				<Switch>
 					<Route exact path="/">
 						<Home />
+					</Route>
+					<Route exact path="/signin">
+						<SignIn />
 					</Route>
 				</Switch>
 			</div>
