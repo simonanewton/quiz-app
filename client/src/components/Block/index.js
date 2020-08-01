786ab4a3379430dae33d72f4e3fdfaf9d460133e
Simon <simonanewton@gmail.com>
2020-08-01 17:31:49 -0400

    removed Link margins from Block component, turned Header component links into React Router Links

--- client/src/components/Block/index.js
+++ client/src/components/Block/index.js
@@ -5,14 +5,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function HomeCard({ link, name, icon }) {
     return (
-        <Link to={link} style={{ textDecoration: "none" }}>
-            <Card bg="primary" text="light" style={{ width: '30rem', height: '16rem' }} className="m-3">
-                <Card.Body className="mx-auto d-flex align-items-center">
-                    <h2 className="pr-3">{name}</h2>
-                    <FontAwesomeIcon icon={icon} size="2x" />
-                </Card.Body>
-            </Card>
-        </Link>
+        <div className="m-3">
+            <Link to={link} style={{ textDecoration: "none" }}>
+                <Card bg="primary" text="light" style={{ width: '30rem', height: '16rem' }}>
+                    <Card.Body className="mx-auto d-flex align-items-center">
+                        <h2 className="pr-3">{name}</h2>
+                        <FontAwesomeIcon icon={icon} size="2x" />
+                    </Card.Body>
+                </Card>
+            </Link>
+        </div>
     );
 }
 
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import { Navbar, ListGroup } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBolt, faHome, faCog, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
@@ -7,21 +8,21 @@ import "./index.css";
 function Header() {
     return (
         <Navbar bg="primary" variant="light" expand="lg" className="p-4">
-            <Navbar.Brand href="/" className="text-white d-flex align-items-center">
+            <Navbar.Brand as={Link} to="/" className="text-white d-flex align-items-center">
                 <h2 className="pr-3">QuizApp</h2>
                 <FontAwesomeIcon icon={faBolt} size="2x" />
             </Navbar.Brand>
 
             <ListGroup horizontal className="ml-auto">
-                <ListGroup.Item action href="/" className="text-primary text-nowrap">
+                <ListGroup.Item action as={Link} to="/" className="text-primary text-nowrap">
                     <span className="pr-2">Home</span>
                     <FontAwesomeIcon icon={faHome} />
                 </ListGroup.Item>
-                <ListGroup.Item action href="/settings" className="text-primary text-nowrap">
+                <ListGroup.Item action as={Link} to="/settings" className="text-primary text-nowrap">
                     <span className="pr-2">Settings</span>
                     <FontAwesomeIcon icon={faCog} />
                 </ListGroup.Item>
-                <ListGroup.Item action href="/signin" className="text-primary text-nowrap">
+                <ListGroup.Item action as={Link} to="/signin" className="text-primary text-nowrap">
                     <span className="pr-2">Sign In</span>
                     <FontAwesomeIcon icon={faSignOutAlt} />
                 </ListGroup.Item>
