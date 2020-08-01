e89c3bea9795551bb9bfd5662b00c66292045539
tonidavisj <toni@tonidavisj.com>
2020-08-01 10:44:30 -0400

    updating the signin

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import {Button, FormGroup, FormControl, Form} from "react-bootstrap";
+import {Button, Form, Container, Row} from "react-bootstrap";
 
 
-const Signup = () => {
+const SignIn = () => {
   //const [username, setUsername] = useState();
-  const [email, setEmail] = useState();
+  const [email, setUsername] = useState();
   const [password, setPassword] = useState();
 
   const handleSubmit = e => {
@@ -15,11 +15,37 @@ const Signup = () => {
 
   return(
     <div>
-      <Form onSubmit={handleSubmit}>
-        
-      </Form>
+      <Container>
+      <h1>Welcome to QuizApp!</h1>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control 
+              type="text" 
+              placeholder="Enter Username" 
+              name='username'
+              onChange={e => setUsername(e.target.value)}
+              />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Control 
+            type="password" 
+            placeholder="Password"
+            name= "password"
+            onChange={e => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <p className="signUpLink">Don't have an account? Sign Up</p>
+            <p className="forgotPasswordLink">Forgot Password</p>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
     </div>
   )
 };
 
-export default Signup;
+export default SignIn;
