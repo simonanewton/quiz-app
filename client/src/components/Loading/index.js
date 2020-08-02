d22da2363d7211061b28355901c78809039b4051
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-08-02 09:54:24 -0400

    adding code for loading screen

---
+++ client/src/components/Loading/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom"
+import { Card } from "react-bootstrap";
+import "./index.css";
+
+function Loading() {
+    return (
+        <div id="loader"><img src=".../assets/icon.png" width="100px" /></div>
+    );
+}
+
+export default HomeCard;
