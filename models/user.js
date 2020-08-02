53771b2f37cdf9ec5a617636c42a00c818716ec2
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-08-02 11:16:33 -0400

    added bcrypt

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ const userSchema = new Schema ({
     lastname: { type: String, required: true },
     username: { type: String, required: true },
     emailAddress: { type: String, required: true, unique: true },
+    password: { type: String, required: true, bcrypt: true },
 })
 
 const User = mongoose.model('User', userSchema);
