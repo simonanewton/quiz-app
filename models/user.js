dbb97728ce0eae8b45baed18d8d4fad743b54cc9
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-08-02 11:11:28 -0400

    adding to user schema

--- models/user.js
+++ models/user.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema ({
-
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    username: { type: String, required: true },
+    emailAddress: { type: String, required: true, unique: true },
 })
 
 const User = mongoose.model('User', userSchema);
