cd8072f7be0acae22b5093e6971ee9b2f7902992
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-08-02 10:31:31 -0400

    installed react loading screen

--- client/src/components/Loading/index.js
+++ client/src/components/Loading/index.js
@@ -1,9 +1,19 @@
 import React from "react";
-import "./index.css";
+import LoadingScreen from 'react-loading-screen';
+// import "./index.css";
 
 function Loading() {
     return (
-        <div id="loader"><img src="./assets/icon.png" width="100px" /></div>
+        <LoadingScreen
+            loading={true}
+            bgColor='white'
+            spinnerColor='orange'
+            logoSrc='./assets/icon.png'
+            textColor='orange'
+            text='example'
+        >
+
+        </LoadingScreen>
     );
 }
 
