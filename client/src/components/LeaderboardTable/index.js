b78cc4c66aedc75328d7ee7527e0cb037e27f7aa
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-08-05 18:07:26 -0400

    adding minor styling to table

--- client/src/components/LeaderboardTable/index.js
+++ client/src/components/LeaderboardTable/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { TableHeaderColumn, BootstrapTable } from 'react-bootstrap-table';
+import './index.css';
 
 class LeaderboardTable extends React.Component {
     render() {
         return (
-            <BootstrapTable>
+            <BootstrapTable className="table">
                 <TableHeaderColumn>Username</TableHeaderColumn>
                 <TableHeaderColumn isKey dataField="id">Score</TableHeaderColumn>
             </BootstrapTable>
