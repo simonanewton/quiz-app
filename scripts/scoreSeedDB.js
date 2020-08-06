d903b6d7d25585b9e491a793f3b233c760b8a8d8
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-08-06 16:34:24 -0400

    finishing seed data?

--- scripts/scoreSeedDB.js
+++ scripts/scoreSeedDB.js
@@ -9,3 +9,15 @@ const scoreSeed = [{
     subject: "English",
     score: 54
 }]
+
+db.Score
+    .remove({})
+    .then(() => db.Score.collection.insertMany(scoreSeed))
+    .then(data => {
+        console.log("score inserted!")
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    })
