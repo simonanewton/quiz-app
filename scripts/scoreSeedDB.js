f9128f9f2a4d1e3eda20818713a959271dfa6734
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-08-06 16:20:13 -0400

    adding seed data

---
+++ scripts/scoreSeedDB.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/quizapp"
+);
+
+const scoreSeed = [{
+    
+}]
