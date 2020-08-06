047d473cfb70676ca59e175ac94d2ed00c624abc
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-08-06 17:18:18 -0400

    adjusting score model

--- models/score.js
+++ models/score.js
@@ -3,9 +3,14 @@ const Schema = mongoose.Schema;
 
 const scoreSchema = new Schema ({
     username: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    subject: { type: String },
-    difficulty: { type: Number },
-    score: { type: Number }
+    subject: [{
+        id: { type: String, required: true },
+        subj: { type: String, required: true },
+        difficulty: [{
+            id: { type: String, required: true },
+            difficulty: { type: Number, required: true }
+        }]
+    }]
 })
 
 const Score = mongoose.model('Score', scoreSchema);
