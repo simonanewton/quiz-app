e3d0ea96efb5a1aadbf94e2f8641c7b83ca14a95
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-08-07 09:48:23 -0400

    starting menu for difficulty

---
+++ client/src/components/LeaderboardMenu/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './index.css';
+
+class LeaderboardMenu extends React.Component {
+    render() {
+        return (
+
+    )};
+}
+
+export default LeaderboardMenu;
