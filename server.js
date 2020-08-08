8bfa8315b5262273a6cfade9517f07f380aac6e2
tonidavisj <toni@tonidavisj.com>
2020-08-08 14:39:43 -0400

    updating branch 2

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-<<<<<<< HEAD
 //const cookieParser = require('cookie-parser');
-=======
-const routes = require('./routes');
-const cookieParser = require('cookie-parser');
->>>>>>> 68fce91338fd49ac683e6ec8b0479eef1432f869
 
 const app = express();
 const PORT = process.env.PORT || 3001;
