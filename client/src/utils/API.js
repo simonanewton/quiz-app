9c92f0f1cad839d3bb9b5e3eb466a1e9a3b807ec
simonanewton <simonanewton@gmail.com>
2020-08-09 22:08:18 -0400

    added initial updateUserScore implementation

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,32 +1,37 @@
-  
-import axios from "axios";
+import axios from 'axios';
 
 export default {
-  // Gets all saved users
-  getUsers: function() {
-    return axios.get("/api/user");
-  },
-  
-  //Get by email
-  getEmail: function(email) {
-    return axios.get("/api/user/" + email);
-  },
-  getById: function(id) {
-    console.log(id)
-    return axios.get("/api/user/" + id)
-  },
-  // Deletes the saved user with the given id
-  deleteUser: function(id) {
-    return axios.delete("/api/user/" + id);
-  },
-  // Adds a user to database
-  createUser: function(userData) {
-    return axios.post("/api/user", userData);
-  },
-  loginUser: function(userData) {
-    return axios.post("/api/signin/", userData)
-  },
-  logoutUser: function() {
-    return axios.post("/api/signout")
-  }
+	// Gets all saved users
+	getUsers: () => {
+		return axios.get("/api/user");
+	},
+	// Get by email
+	getEmail: (email) => {
+		return axios.get("/api/user/" + email);
+	},
+	// Gets user data by id number
+	getById: (id) => {
+		console.log(id)
+		return axios.get("/api/user/" + id);
+	},
+	// Deletes the saved user with the given id
+	deleteUser: (id) => {
+		return axios.delete("/api/user/" + id);
+	},
+	// Adds a user to database
+	createUser: (userData) => {
+		return axios.post("/api/user", userData);
+	},
+	// Authenticates user login
+	loginUser: (userData) => {
+		return axios.post("/api/signin/", userData);
+	},
+	// Logs out the current user
+	logoutUser: () => {
+		return axios.post("/api/signout");
+	},
+	// Updates the users score with a new highscore
+	updateUserScore: (id, score, difficulty, subject) => {
+		return axios.put("/api/user/" + id, (score, difficulty, subject));
+	}
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,32 +3,39 @@ const db = require("../models");
 const { request, response } = require("express");
 
 module.exports = {
+	// find all users by score, sort by ascending
+	findAll: (req, res) => {
+		db.User
+			.find(req.query) // find all
+			.limit(10) // limit to 10 users
+			.sort({ scores: -1 }) // sort them in descending order
+			.then(dbUser => res.json(dbUser))
+			.catch(err => res.status(422).json(err));
+	},
 
-  // find all users by score, sort by ascending
-  findAll: function(req, res) {
-    db.User
-      .find(req.query) // find all
-      .limit(10) // limit to 10 users
-      .sort({ scores: -1 }) // sort them in descending order
-      .then(dbUser => res.json(dbUser))
-      .catch(err => res.status(422).json(err));
-  },
+	// find all by id
+	findById: (req, res) => {
+		db.User
+			.findById(req.params.id)
+			.then(dbUser => res.json(dbUser))
+			.catch(err => res.status(422).json(err));
+	},
 
-  // find all by id
-  findById: function(req, res) {
-    db.User
-      .findById(req.params.id)
-      .then(dbUser => res.json(dbUser))
-      .catch(err => res.status(422).json(err));
-  },
+	// creating new users/posting to json
+	create: (req, res) => {
+		console.log(req.body);
+		db.User
+			.create(req.body)
+			.then(data => res.json(data))
+			.catch(err => res.status(422).json(err));
+	},
 
-  // creating new users/posting to json
-  create: function(req, res) {
-    console.log(req.body);
-    db.User
-    .create(req.body)
-    .then(data => res.json(data))
-    .catch(err => res.status(422).json(err));
-  }
-
+	updateScore: (req, res) => {
+		console.log(req.body);
+		res.status(200);
+		// db.User
+		// 	.update(req.body)
+		// 	.then(data => res.json(data))
+		// 	.catch(err => res.status(422).json(err));
+	}
 }
